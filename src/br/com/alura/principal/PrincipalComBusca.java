30a50eb604cc2a96685e3138dbda972bedec9864
Digonil <diego.niles@hotmail.com>
2023-04-22 20:44:05 -0300

    Criado arquivos para o armazenamento de dados oriundos de um json

--- src/br/com/alura/principal/PrincipalComBusca.java
+++ src/br/com/alura/principal/PrincipalComBusca.java
@@ -5,11 +5,14 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -18,38 +21,49 @@ public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Locale.setDefault(Locale.US);
+
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um filme para busca:");
 
-        var busca = scanner.nextLine();
-        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=db6c566c";
-        try {
-            HttpClient cliente = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        while (!busca.equalsIgnoreCase("sair")) {
 
-            HttpResponse<String> response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
-            String json = response.body();
-            System.out.println(json);
+            System.out.println("Digite um filme para busca:");
+            busca = scanner.nextLine();
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=db6c566c";
+            try {
+                HttpClient cliente = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
 
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println("Titulo: " + meuTituloOmdb);
+                HttpResponse<String> response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
+                String json = response.body();
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
 
 //        try{
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println("Acontecue um erro");
-            System.out.println(meuTitulo);
-        } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Título já convertido");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Algum erro de argumento na busca, verifique o endereço!");
-        }catch (ErroDeConversaoDeAnoException e){
-            System.out.println(e.getMessage());
-        }
-        System.out.println("O programa finalizou corretamente!");
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                titulos.add(meuTitulo);
 
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Título já convertido");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço!");
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMessage());
+            }
+            FileWriter escrita = new FileWriter("filmes.json");
+            escrita.write(gson.toJson(titulos));
+            escrita.close();
+
+            System.out.println("O programa finalizou corretamente!");
+
+        }
 
     }
 }
